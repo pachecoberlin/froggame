ae161ec0884cce0df46431586c36e00845740699
pacheco <pacheco@gmx.de>
2024-01-30 20:57:27 +0100

    initial commit

---
+++ feature-frogdata/src/main/java/de/pacheco/froggame/feature/frogdata/ui/FrogDataViewModel.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.pacheco.froggame.feature.frogdata.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import de.pacheco.froggame.core.data.FrogDataRepository
+import de.pacheco.froggame.feature.frogdata.ui.FrogDataUiState.Error
+import de.pacheco.froggame.feature.frogdata.ui.FrogDataUiState.Loading
+import de.pacheco.froggame.feature.frogdata.ui.FrogDataUiState.Success
+import javax.inject.Inject
+
+@HiltViewModel
+class FrogDataViewModel @Inject constructor(
+    private val frogDataRepository: FrogDataRepository
+) : ViewModel() {
+
+    val uiState: StateFlow<FrogDataUiState> = frogDataRepository
+        .frogDatas.map<List<String>, FrogDataUiState> { Success(data = it) }
+        .catch { emit(Error(it)) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), Loading)
+
+    fun addFrogData(name: String) {
+        viewModelScope.launch {
+            frogDataRepository.add(name)
+        }
+    }
+}
+
+sealed interface FrogDataUiState {
+    object Loading : FrogDataUiState
+    data class Error(val throwable: Throwable) : FrogDataUiState
+    data class Success(val data: List<String>) : FrogDataUiState
+}
