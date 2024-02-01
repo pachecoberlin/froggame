3b958dcf8ba03a31e4780eb5f81fba8dc1108a2c
pacheco <pacheco@gmx.de>
2024-02-01 20:13:24 +0100

    enableEdgeToEdge

--- app/src/main/java/de/pacheco/froggame/ui/MainActivity.kt
+++ app/src/main/java/de/pacheco/froggame/ui/MainActivity.kt
@@ -19,6 +19,7 @@ package de.pacheco.froggame.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,6 +34,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             FrogMainTheme {
                 Surface(
