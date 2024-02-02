af07663d3810756ea7be19ef8dcdb3f8e9d95876
pacheco <pacheco@gmx.de>
2024-02-02 22:38:19 +0100

    Added frogs to CatchFrogScreen

--- feature-catchFrog/src/main/java/de/pacheco/feature/ui/CatchFrogScreen.kt
+++ feature-catchFrog/src/main/java/de/pacheco/feature/ui/CatchFrogScreen.kt
@@ -1,47 +1,87 @@
 package de.pacheco.feature.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import de.pacheco.feature.catchfrog.R
+import de.pacheco.froggame.core.ui.DevicePreviews
 import de.pacheco.froggame.core.ui.FrogMainTheme
 
 @Composable
 fun CatchFrogScreen(modifier: Modifier = Modifier) {
     CatchFrogScreen(
-        items = listOf("one", "two"),
+        6 to 6,
         modifier = modifier
     )
-
 }
 
 @Composable
 internal fun CatchFrogScreen(
-    items: List<String>,
+    size: Pair<Int, Int>,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier) {
-        items.forEach {
-            Text(it)
+    Column(modifier.verticalScroll(state = ScrollState(0))) {
+        Row(modifier) {
+            Text(text = "Time", modifier = modifier)
+            Column(Modifier.weight(1f)) {}
+            Button(onClick = { replay() }, modifier = modifier) {
+                Text(text = "play again")
+            }
+        }
+        CatchFrogsMatrix(columns = size.first, rows = size.second)
+        Row(modifier = modifier.padding(bottom = 30.dp)) {
+            Text(text = "Score: 0")
+            Column(Modifier.weight(1f)) {}
+            Text(text = "HighScore: 0", modifier = Modifier.padding(start = 10.dp))
         }
     }
 }
 
-// Previews
-
-@Preview(showBackground = true)
 @Composable
-private fun DefaultPreview() {
-    FrogMainTheme {
-        CatchFrogScreen(listOf("one", "two"))
+private fun CatchFrogsMatrix(columns: Int, rows: Int) {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        for (i in 0..<columns) {
+            Column {
+                CatchableFrogsRows(rows)
+            }
+            Column(Modifier.weight(1f)) {}
+        }
     }
 }
 
-@Preview(showBackground = true, widthDp = 480)
 @Composable
-private fun PortraitPreview() {
+private fun CatchableFrogsRows(rows: Int) {
+    for (i in 0..<rows) {
+        Row {
+            CatchableFrog()
+        }
+    }
+}
+
+@Composable
+fun CatchableFrog() {
+    Image(painter = painterResource(id = R.drawable.frog), contentDescription = "a frog to catch")
+}
+
+private fun replay() {}
+
+// Previews
+
+@DevicePreviews
+@Composable
+private fun DefaultPreview() {
     FrogMainTheme {
-        CatchFrogScreen(listOf("one", "two"))
+        CatchFrogScreen()
     }
 }
