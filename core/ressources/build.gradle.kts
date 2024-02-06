76002afbb829b0865c0463bd97e2c300dfed6a7d
pacheco <pacheco@gmx.de>
2024-02-06 22:14:24 +0100

    Added resources module

---
+++ core/ressources/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "de.pacheco.froggame.core.ressources"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.appcompat)
+//    implementation(libs.material)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.test.ext.junit)
+//    androidTestImplementation(libs.espresso.core)
+}
