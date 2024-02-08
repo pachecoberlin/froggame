1df29c2397905d4bcdee99a0304be1717b308b8f
pacheco <pacheco@gmx.de>
2024-02-08 22:55:48 +0100

    formatted and cleaned

--- core/data/src/main/java/de/pacheco/froggame/core/data/repos/interfaces/ICatchFrogRepository.kt
+++ core/data/src/main/java/de/pacheco/froggame/core/data/repos/interfaces/ICatchFrogRepository.kt
@@ -1,9 +1,7 @@
 package de.pacheco.froggame.core.data.repos.interfaces
 
-import kotlinx.coroutines.flow.Flow
-
 interface ICatchFrogRepository {
     fun clearHighScore()
     suspend fun startGame(numberOfFrogs: Int): Int
-    fun stopGame(highscore:Int)
+    fun stopGame(highscore: Int)
 }
