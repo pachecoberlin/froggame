a5e6426c38d8f6dca9c18b69999e159b04a279a3
pacheco <pacheco@gmx.de>
2024-02-08 16:47:07 +0100

    Removed unnecessary scope.launch

--- feature/catchFrog/src/main/java/de/pacheco/froggame/feature/catchfrog/ui/CatchFrogViewModel.kt
+++ feature/catchFrog/src/main/java/de/pacheco/froggame/feature/catchfrog/ui/CatchFrogViewModel.kt
@@ -11,8 +11,8 @@ import javax.inject.Inject
 class CatchFrogViewModel @Inject constructor(
     private val startGameUseCase: StartGameUseCase
 ) : ViewModel() {
-    val startGame: Pair<Function, (params: Map<Parameter, Any>) -> Unit> = Function.STARTGAME to { params ->
-        viewModelScope.launch { startGameUseCase(params[Parameter.ROWS] as? Int ?: 4, params[Parameter.COLS] as? Int ?: 4) }
+    private val startGame: Pair<Function, (params: Map<Parameter, Any>) -> Unit> = Function.STARTGAME to { params ->
+            startGameUseCase(params[Parameter.ROWS] as? Int ?: 4, params[Parameter.COLS] as? Int ?: 4)
     }
 
     val functions: Map<Function, (params: Map<Parameter, Any>) -> Unit> = mapOf(startGame)
