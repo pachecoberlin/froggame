552a57163c076a24b0a4769e56ba848f2167f874
pacheco <pacheco@gmx.de>
2024-02-08 16:46:42 +0100

    Using CatchFrogEngine

--- core/domain/src/main/java/de/pacheco/froggame/core/domain/usecases/StartGameUseCase.kt
+++ core/domain/src/main/java/de/pacheco/froggame/core/domain/usecases/StartGameUseCase.kt
@@ -1,11 +1,11 @@
 package de.pacheco.froggame.core.domain.usecases
 
-import de.pacheco.froggame.core.data.repos.interfaces.ICatchFrogRepository
+import de.pacheco.froggame.core.domain.game.engine.ICatchFrogEngine
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class StartGameUseCase @Inject constructor(private val catchFrogRepository: ICatchFrogRepository, private val catchFrogEngine:ICatchFrogEngine) {
-    suspend operator fun invoke(rows:Int, cols:Int): Flow<List<Int>> {
-        return catchFrogEngine.startGame(rows*cols)
+class StartGameUseCase @Inject constructor(private val catchFrogEngine: ICatchFrogEngine) {
+    operator fun invoke(rows: Int, cols: Int): Flow<Int> {
+        return catchFrogEngine.startGame(rows * cols)
     }
 }
