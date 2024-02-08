93eb38cc20fbd2820929c02fce37ad98df0deacf
pacheco <pacheco@gmx.de>
2024-02-08 22:51:37 +0100

    CatchFrogEngine added getGameData method and implemented

--- core/domain/src/main/java/de/pacheco/froggame/core/domain/game/engine/CatchFrogEngine.kt
+++ core/domain/src/main/java/de/pacheco/froggame/core/domain/game/engine/CatchFrogEngine.kt
@@ -1,23 +1,42 @@
 package de.pacheco.froggame.core.domain.game.engine
 
 import de.pacheco.froggame.core.data.repos.interfaces.ICatchFrogRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.concurrent.timer
 import kotlin.random.Random
 
 class CatchFrogEngine @Inject constructor(private val catchFrogRepository: ICatchFrogRepository) : ICatchFrogEngine {
+    private var frogCount: Int = 1
     private var actualScore: Int = 0
     private var delay: Long = 300
 
-    override fun startGame(frogCount: Int): Flow<Int> {
+    override fun getGameData(): Flow<Int> {
         return flow {
-            while (true) {
-                emit(Random.nextInt(0, frogCount))
+            var running = true
+            timer(initialDelay = 10000L, period = 1L) {
+                running = false
+                this.cancel()
+            }
+            while (running) {
+                emit(Random.nextInt(frogCount))
                 delay(delay)
             }
+            emit(-1)
+            val job = SupervisorJob()
+            val scope = CoroutineScope(Dispatchers.IO + job)
+            scope.launch { stopGame() }
         }
+    }
+
+    override suspend fun startGame(frogCount: Int) {
+        this.frogCount = frogCount
     }
 
     override suspend fun stopGame() {
--- core/domain/src/main/java/de/pacheco/froggame/core/domain/game/engine/ICatchFrogEngine.kt
+++ core/domain/src/main/java/de/pacheco/froggame/core/domain/game/engine/ICatchFrogEngine.kt
@@ -3,6 +3,7 @@ package de.pacheco.froggame.core.domain.game.engine
 import kotlinx.coroutines.flow.Flow
 
 interface ICatchFrogEngine {
-  fun startGame(frogCount:Int): Flow<Int>
+  fun getGameData(): Flow<Int>
+  suspend fun startGame(frogCount:Int)
   suspend fun stopGame()
 }
