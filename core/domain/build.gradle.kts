32639aea620f5da56850acfeae98208e7aa3f60b
pacheco <pachecoberlin@gmail.com>
2025-03-15 20:22:38 +0100

    updated to Java 21 and gradle Wrapper

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         minSdk = 21
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
--- core/utils/build.gradle.kts
+++ core/utils/build.gradle.kts
@@ -11,8 +11,11 @@ android {
         minSdk = 21
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlinOptions {
+        jvmTarget = "21"
     }
 }
 
